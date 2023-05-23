014451974d5f832636c3acc609641231a77b73fb
Andri nova riswanto <anvarisy@gmail.com>
2023-05-23 13:50:38 +0700

    Update model.go

--- model.go
+++ model.go
@@ -8,19 +8,25 @@ type Data struct {
 	Device    Device     `json:"device"`
 }
 
+type Geolocation struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
 type Device struct {
-	Score           float64  `json:"score"`
-	Platform        string   `json:"platform"`
-	FazpassId       string   `json:"fazpass_id"`
-	IsRooted        bool     `json:"is_rooted"`
-	IsEmulator      bool     `json:"is_emulator"`
-	IsGpsSpoof      bool     `json:"is_gps_spoof"`
-	IsAppTemper     bool     `json:"is_app_temper"`
-	IsVpn           bool     `json:"is_vpn"`
-	IsScreenSharing bool     `json:"is_share_screen"`
-	IsDebuging      bool     `json:"is_debuging"`
-	Name            string   `json:"name"`
-	SimSerial       []string `json:"sim_serial"`
+	Score           float64     `json:"score"`
+	Platform        string      `json:"platform"`
+	FazpassId       string      `json:"fazpass_id"`
+	IsRooted        bool        `json:"is_rooted"`
+	IsEmulator      bool        `json:"is_emulator"`
+	IsGpsSpoof      bool        `json:"is_gps_spoof"`
+	IsAppTemper     bool        `json:"is_app_temper"`
+	IsVpn           bool        `json:"is_vpn"`
+	IsScreenSharing bool        `json:"is_share_screen"`
+	IsDebuging      bool        `json:"is_debuging"`
+	Name            string      `json:"name"`
+	SimSerial       []string    `json:"sim_serial"`
+	Geolocation     Geolocation `json:"geolocation"`
 }
 
 type Transmission struct {
